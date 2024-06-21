f69f036e71cab6f96af9f27c22dd6bf6752b3b5c
ComwrapUkReply <s.sznajder@reply.com>
2024-06-21 12:07:00 +0100

    add dynamic cta and columns

---
+++ blocks/dynamic-cta/dynamic-cta.js
@@ -0,0 +1,20 @@
+export default function decorate(block) {
+    const cols = [...block.children];
+    cols.forEach((col, index) => {
+        if (index === 0){
+            col.classList.add('dynamic-img');
+        } else {
+            col.classList.add('dynamic-content');
+        }
+        
+        const par = col.querySelectorAll('p');
+        par.forEach((p, index) => {
+            if (index === 0){
+                p.classList.add('content-img');
+            } else {
+                p.classList.add('conetnt-content');
+            }
+        });
+
+    });
+}
