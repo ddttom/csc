fc97ecc0bc31bf4cea66fc6fe6c9ef6f06d6be44
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-06-22 09:19:03 +0100

    feat: Add dynamic classes and elements to dynamic-hero.js

--- blocks/dynamic-hero/dynamic-hero.js
+++ blocks/dynamic-hero/dynamic-hero.js
@@ -1,31 +1,31 @@
-export default function decorate(block){
-    // Get all DIV children of the block
-    const divEl = [...block.children].filter(child => child.tagName === 'DIV');
-    
-    if(divEl.length >= 3) {
-        // Add classes to the first three div elements
-        divEl[0].classList.add('dynamic-one');
-        divEl[1].classList.add('dynamic-two');
-        divEl[2].classList.add('dynamic-three');
+export default function decorate(block) {
+  // Get all DIV children of the block
+  const divEl = [...block.children].filter((child) => child.tagName === 'DIV');
 
-        const pElement = [...divEl[1].querySelectorAll('p')];
-        pElement.forEach((p, index) => {
-            p.classList.add(`p-${index}`);
-            if (index === 1) {
-                const h1 = document.createElement('h1');
-                h1.textContent = p.textContent;
-                p.replaceWith(h1)
-            } else if (index === 2 ) {
-                const h2 = document.createElement('h2');
-                h2.textContent = p.textContent;
-                p.replaceWith(h2)
-            }
-        });
+  if (divEl.length >= 3) {
+    // Add classes to the first three div elements
+    divEl[0].classList.add('dynamic-one');
+    divEl[1].classList.add('dynamic-two');
+    divEl[2].classList.add('dynamic-three');
 
-        const pZero = document.querySelector('.p-0');
+    const pElement = [...divEl[1].querySelectorAll('p')];
+    pElement.forEach((p, index) => {
+      p.classList.add(`p-${index}`);
+      if (index === 1) {
+        const h1 = document.createElement('h1');
+        h1.textContent = p.textContent;
+        p.replaceWith(h1);
+      } else if (index === 2) {
+        const h2 = document.createElement('h2');
+        h2.textContent = p.textContent;
+        p.replaceWith(h2);
+      }
+    });
 
-        const parent = pZero.parentNode;
-        parent.classList.add('content')
-        parent.insertAdjacentElement('afterend', pZero);
-    }
+    const pZero = document.querySelector('.p-0');
+
+    const parent = pZero.parentNode;
+    parent.classList.add('content');
+    parent.insertAdjacentElement('afterend', pZero);
+  }
 }
