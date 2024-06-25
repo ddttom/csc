39509a53ce4c56ad48015251b045c26c34ef48e1
ComwrapUkReply <s.sznajder@reply.com>
2024-06-25 15:22:55 +0100

    formatting fix

--- blocks/dynamic-cta/dynamic-cta.js
+++ blocks/dynamic-cta/dynamic-cta.js
@@ -21,23 +21,3 @@ export default function decorate(block) {
   });
   updateDynamicImage('.dynamic-img', 2);
 }
-=======
-	const cols = [...block.children];
-	cols.forEach((col, index) => {
-		if (index === 0){
-			col.classList.add('dynamic-img');
-	} else {
-			col.classList.add('dynamic-content');
-		}
-
-		const par = col.querySelectorAll('p');
-		par.forEach((p, index) => {
-			if (index === 0){
-				p.classList.add('content-img');
-			} else {
-				p.classList.add('conetnt-content');
-			}
-		});
-
-	});
-}
