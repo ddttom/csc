c8e6164080dbece895c4de30d1bdf99ffa9d0d9c
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-06-26 13:40:50 +0100

    Refactor equality comparisons to use ==
    The strict equality operator (===) was replaced with the
    equality operator (==) in two conditional checks. This
    change loosens the comparison to allow for type coercion.
    
    While often discouraged, using == may be acceptable here
    if the code expects the types to potentially differ but
    wants to treat them as equal (e.g. string '0' and number 0).

--- blocks/dynamic-cta/dynamic-cta.js
+++ blocks/dynamic-cta/dynamic-cta.js
@@ -4,7 +4,7 @@
 export default function decorate(block) {
   const cols = [...block.children];
   cols.forEach((col, index) => {
-    if (index === 0) {
+    if (index == 0) {
       col.classList.add('dynamic-img');
     } else {
       col.classList.add('dynamic-content');
@@ -12,7 +12,7 @@ export default function decorate(block) {
 
     const par = col.querySelectorAll('p');
     par.forEach((p, qindex) => {
-      if (qindex === 0) {
+      if (qindex == 0) {
         p.classList.add('content-img');
       } else {
         p.classList.add('content-content');
